bfdaf18c96da82175922ea76cda228caec5a26a9
Felix Schaphaus <felix.schaphaus@web-computing.de>
2025-03-04 08:20:19 +0100

    fix gpg key for apt

--- pkg/scripts/os_debian.go
+++ pkg/scripts/os_debian.go
@@ -72,7 +72,7 @@ sudo systemctl enable --now iscsid
 {{- if .CONFIGURE_REPOSITORIES }}
 sudo install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
